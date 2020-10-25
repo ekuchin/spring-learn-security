96c5c71a41716c7d45bff459e5eed2ad6d335b77
Eugene A. Kuchin <kea@project-osnova.ru>
2020-10-26 02:55:40 +0500

    Return cats list

--- src/main/java/ru/projectosnova/springlearnsecurity/entity/Cat.java
+++ src/main/java/ru/projectosnova/springlearnsecurity/entity/Cat.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.io.Serializable;
 
 @Entity
 public class Cat{
